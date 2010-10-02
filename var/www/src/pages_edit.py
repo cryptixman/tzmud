5792ea37e23ab4b71dd6753844358d064890fa86
missive <missive@hotmail.com>
2010-10-02 21:02:05 +0000

    make Add Exit to Room form narrower

--- var/www/src/pages_edit.py
+++ var/www/src/pages_edit.py
@@ -388,20 +388,22 @@ class Edit(pages_base.TZPage):
                             choices=choices,
                             selected='Exit')
         row = T.tr[T.td['Exit type...', T.br,
-                            self.render_form_select(xinfo)],
-                    T.td['named...', T.br, T.input(name='xname')],
+                            self.render_form_select(xinfo), T.br,
+                            'named...', T.br,
+                            T.input(name='xname')],
                     T.td['to...', T.br, '-->'],
                     T.td(align="center")[self.rooms_widget('dest', None),
                             T.br, 'or', T.br,
                             self.new_room_widget('newroom'), T.br,
+                            'named...', T.br,
                             T.input(name='newroomname'), T.br,
                             T.input(type='submit', value=' Add ')],
                     T.td[T.br, '<--'],
                     T.td['return by...', T.br,
-                            T.input(name='bxname')],
-                    T.td['Exit type...', T.br,
-                            self.render_form_select(bxinfo)],
-                    T.td[T.input(type='submit', value=' Add ')]]
+                            'Exit type...', T.br,
+                            self.render_form_select(bxinfo), T.br,
+                            'named...', T.br,
+                            T.input(name='bxname')]]
         tbl = T.table(_class="center")[row]
         lines.append(tbl)
         lines.append(T.input(_type="hidden", name="roomid", value=tzid))
